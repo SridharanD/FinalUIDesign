b3c81e4ce695ef30402638c1b93442bc26f3172e
sridharan1122@outlook.com <sridharan1122@outlook.com>
2022-03-22 07:16:58 +0530

    before normal form

--- src/components/Client/ClientConfig.vue
+++ src/components/Client/ClientConfig.vue
@@ -1,104 +1,130 @@
 <template>
-  <div> 
-        <div><center>
-            <form >
-                 <md-card class="md-layout-item md-size-50 md-small-size-100">
+  <div>
+    <div>
+      <center>
+        <form id='newform'>
+          <md-card class="md-layout-item md-size-50 md-small-size-100">
         <md-card-header>
           <div class="md-title">Configuration File</div>
         </md-card-header>
 
-        <md-card-content>
-           <div >
-        <md-field>
-          <label for="clientname">Client Name</label>
-          <md-select v-model="clientname" name="clientname" id="clientname">
-            <md-option v-for="item in info"  :value='item.name' :key='item' >{{item.name}}</md-option>
-          </md-select>
-        </md-field>
-      </div>
+            <md-card-content>
+              <table>
+                <tr>
+                 
+                    <th><label for="clientname">Client Name</label></th>
+                    <td>
+                      <md-field>
+                        <md-select
+                          v-model="clientname"
+                        >
+                          <md-option
+                            v-for="(item,index) in info"
+                            :value="item.name"
+                            :key="index"
+                            >{{ item.name}}</md-option
+                          >
+
+                        </md-select>
+                      </md-field>
+                    </td>
+                </tr>
+
+                <tr>
+                  
+                    <th><label for="LocationName">Location Name</label></th>
+                    <td>
+                      <md-field>
+                        <md-select
+                          v-model="OrgLocation"
+                         
+                        >
+                          <md-option
+                            v-for="(items,index) in infodetails"
+                            :value="items.locationName"
+                            :key="index"
+                            >{{ items.locationName }}</md-option
+                          >
+                        </md-select>
+                      </md-field>
+                    </td>
+                </tr>
+                <tr>
+                  
+                    <th><label for="Device Name">Device Name</label></th>
+                    <td>
+                      <md-field>
+                        <md-select v-model="devName" id="devicename">
+                          <md-option
+                            v-for="(dataitem,index) in details"
+                            :value="dataitem.deviceName"
+                            :key="index"
+                            >{{ dataitem.deviceName }}
+                          </md-option>
+                        </md-select>
+                      </md-field>
+                    </td>
+                  
+                </tr>
+                <tr><th>
+                  <button id='button-1' v-on:click="reset()">Cancel</button>     
+                    </th>
+                  <th>
+                  <button id='button-2' v-on:click="Download()">Download</button>
+                  </th>
 
 
-    <div >
-        <md-field>
-          <label for="LocationName">Location Name</label>
-          <md-select v-model="OrgLocation" name="clientname" id="clientname">
-            <md-option v-for="items in infodetails"  :value='items.locationName' :key='items.deviceID' >{{items.locationName}}</md-option>
-          </md-select>
-        </md-field>
+                </tr>
+              </table>
+            </md-card-content>
+          </md-card>
+        </form>
+      </center>
     </div>
-
-    <div >
-        <md-field>
-          <label for="Device Name">Device Name</label>
-          <md-select v-model="devName" id="devicename">
-            <md-option v-for="dataitem in details"  :value='dataitem.deviceName' :key='dataitem.deviceID'  >{{dataitem.deviceName}} </md-option>
-          </md-select>
-        </md-field>
-    </div>
-    
-    <md-button class="md-raised md-primary" v-on:click="Download()">Download</md-button>
-
-
-</md-card-content>
-
-</md-card>
-</form>
-</center>
-
-</div>
-
-</div>
-
-       
-
-
-
-
-  
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
-
+import axios from "axios";
+import SelectInput from "../Client/SelectInput.vue";
 
 export default {
-    name:"ClientConfig",
-data:function() {
+  name: "ClientConfig",
+  data: function () {
     return {
+      clientname: "",
+      details: [],
+      info: {},
+      infodetails: [],
+      IsVisible: true,
+      OrgLocation: null,
+      devName: "",
+      data: {},
+    };
+  },
+  created() {
+    axios.get("http://localhost:6060/api/Home/getClientName").then((res) => {
+      this.info = res.data;
+      //console.log(this.info)
+    });
+  },
 
-      clientname:'',
-      details:[],
-      info:{},
-      infodetails:{},
-      IsVisible:true,
-      OrgLocation:null,
-      devName:'', 
-      data:{}  
-    }
-}, 
-created(){
+  methods: {
+    reset:function(){
+       document.getElementById("newForm").reset();
+    },
+    Download: function () {
+      this.details = this.details.filter(
+        (x) => x.deviceName == this.devName
+      );
+      //console.log("this is the sample",this.details);
 
-   axios.get('http://localhost:6060/api/Home/getClientName')
-          .then((res)=>{
-           this.info=res.data;
-           //console.log(this.info)
-        }
-        )
-       
-},
+      // var m = module.globeDeployment
+      // m = this.details
 
- methods : {
-   
-    Download:function(){
-        this.details=this.infodetails.filter(x=> x.deviceName== this.devName);
-        //console.log("this is the sample",this.details);
-        
-        // var m = module.globeDeployment
-        // m = this.details
-
-var data = `module.exports = {
-globeIdentification:"`+this.details[0].deviceName+`",
-globeDeployment: "`+this.details[0].clientName+" "+this.details[0].locationName+`",
+      var data =`module.exports = {
+globeIdentification:"` +this.details[0].deviceName +`",
+globeDeployment: "` +this.details[0].clientName+" "+this.details[0].locationName +`",
 name: "GlobeChek_DESKTOP",
 version: "6.0.13",
 environment: "dev",
@@ -107,14 +133,13 @@ ftpuser: "ftpuser",
 ftppsw: "GlobeChekPass1!",
 ftphost: "caecvm.eastus2.cloudapp.azure.com",
 urlPrereg:
-"https://globecheckportal-globecheckapi.azurewebsites.net/api/v1/Desktop/api/v1/Desktop/`+this.details[0].clientID+"~"+this.details[0].deviceID +`/",
+"https://globecheckportal-globecheckapi.azurewebsites.net/api/v1/Desktop/api/v1/Desktop/`+this.details[0].clientID+"~"+this.details[0].deviceID+`/",
 urlPortal: "https://gcdevstor.z20.web.core.windows.net/",
 urlAPI:"https://globecheckportal-globecheckapi.azurewebsites.net/api/v1/Desktop/`+this.details[0].clientID+"~"+this.details[0].deviceID +`/",
 urlCompanyWeb: "https://www.globechek.com/",
 va: false,
 maxHeight: "73",
-};`
-
+};`;
 
       //var all = new datamodel();
       // all.globeIdentification = this.details[0].deviceName;
@@ -122,42 +147,147 @@ maxHeight: "73",
       // all.urlPrereg = "https://globecheckportal-prod-api.azurewebsites.net/api/v1/Desktop/"+this.details[0].clientID+'~'+this.details[0].deviceID+"/";
       // all.urlAPI = "https://globecheckportal-prod-api.azurewebsites.net/api/v1/Desktop/"+this.details[0].clientID+`~`+this.details[0].deviceID+"/";
 
-      var file = new File([ data  ], { "type" : "" });
-      let link = document.createElement('a')
-      link.href = window.URL.createObjectURL(file)
-      link.download = 'active.config.js'
-      link.click()
+      var file = new File([data], { type: "" });
+      let link = document.createElement("a");
+      link.href = window.URL.createObjectURL(file);
+      link.download = "active.config.js";
+      link.click();
       //console.log("answer",all)
+    },
+  },
 
+  watch: {
+    clientname() {
+      axios
+        .get("http://localhost:6060/api/Home/getGlobeDetails", {
+          params: {
+            ClientName: this.clientname,
+          },
+        })
+        .then((res) => {
+          this.infodetails = res.data;
+        });
+      if (this.infodetails.locationName == null) {
+        console.log("invalid client name");
+      }
     },
 
-},
-
-watch:{
-      
-   clientname(){
-        axios.get('http://localhost:6060/api/Home/getGlobeDetails',{
-            params:{
-                ClientName: this.clientname,
-            }
-        }).then((res)=>{
-           this.infodetails=res.data;})
-           },
-
-    OrgLocation(){
-        return this.details=this.infodetails.filter(x=> x.locationName== this.OrgLocation);
+    OrgLocation() {
+      return (this.details = this.infodetails.filter(
+        (x) => x.locationName == this.OrgLocation
+      ));
+      console.log(details);
     },
-
-},  
-
-
-}
-
-
-
+  },
+};
 </script>
 
 <style>
+.md-title{
+text-align: initial;
+margin-top: -9px;
+}
 
+
+#button-2 {
+  align-items: center;
+  background-color: #0A66C2;
+  border-color: #0A66C2;
+  border-radius: 100px;
+  box-sizing: border-box;
+  color: #ffffff;
+  cursor: pointer;
+  display: inline-flex;
+  font-family: -apple-system, system-ui, system-ui, "Segoe UI", Roboto, "Helvetica Neue", "Fira Sans", Ubuntu, Oxygen, "Oxygen Sans", Cantarell, "Droid Sans", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Lucida Grande", Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+  justify-content: center;
+  line-height: 20px;
+  max-width: 480px;
+  min-height: 40px;
+  min-width: 0px;
+  overflow: hidden;
+  padding: 0px;
+  padding-left: 20px;
+  padding-right: 20px;
+  text-align: center;
+  touch-action: manipulation;
+  transition: background-color 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s, box-shadow 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s, color 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s;
+  user-select: none;
+  -webkit-user-select: none;
+  vertical-align: middle;
+}
+#button-2:hover,
+#button-2:focus { 
+  background-color: #ffffff;
+  color: #0A66C2;
+}
+#button-2:active {
+  background: #09223b;
+  color: rgb(255, 255, 255, .7);
+}
+#button-2:disabled { 
+  cursor: not-allowed;
+  background: rgb(255, 255, 255);
+  color: rgba(255, 254, 254, 0.994);
+}
+
+#button-1 {
+  align-items: center;
+  background-color: #ffffff;
+  border-color: #444444;
+  border-radius: 100px;
+  box-sizing: border-box;
+  color: #444444;
+  cursor: pointer;
+  display: inline-flex;
+  font-family: -apple-system, system-ui, system-ui, "Segoe UI", Roboto, "Helvetica Neue", "Fira Sans", Ubuntu, Oxygen, "Oxygen Sans", Cantarell, "Droid Sans", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Lucida Grande", Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+  justify-content: center;
+  line-height: 20px;
+  max-width: 480px;
+  min-height: 40px;
+  min-width: 0px;
+  overflow: hidden;
+  padding: 0px;
+  padding-left: 20px;
+  padding-right: 20px;
+  text-align: center;
+  touch-action: manipulation;
+  transition: background-color 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s, box-shadow 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s, color 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s;
+  user-select: none;
+  -webkit-user-select: none;
+  vertical-align: middle;
+}
+#button-1:hover,
+#button-1:focus { 
+  background-color: #d4d1d1;
+  color: #1d1d1d;
+}
+#button-1:active {
+  background: #09223b;
+  color: rgb(255, 255, 255, .7);
+}
+#button-1:disabled { 
+  cursor: not-allowed;
+  background: rgb(255, 255, 255);
+  color: rgba(255, 254, 254, 0.994);
+}
+
+ md-select {
+  padding: 12px;
+  border: 10px solid #ccc;
+  border-radius: 10px;
+  resize: vertical;
+  }
+
+label
+{
+  font-weight: 500;
+  padding-bottom: 5px;
+    display: block;
+    text-align: left;
+}
 
 </style>
---
+++ src/components/Client/demo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="md-layout">
+            <div class="md-layout-item">
+                <h3 class="md-title m-0">Client COnfigure</h3>
+            </div>
+        </div>
+    </div>
+
+
+
+</template>
+<script>
+
+
+export default {
+ name: demo  
+}
+</script>
